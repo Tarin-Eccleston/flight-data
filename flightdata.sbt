19227bbd8f942b75d4715f4226cbdcc8feea302b
Tarin <tarin.eccleston@gmail.com>
2022-03-26 13:35:00 +1300

    Update

--- flightdata.sbt
+++ flightdata.sbt
@@ -2,6 +2,6 @@ name := "Flight Data"
 
 version := "1.0"
 
-scalaVersion := "2.13.8"
+scalaVersion := "2.12.15"
 
 libraryDependencies += "org.apache.spark" %% "spark-core" % "3.2.1"
--- src/main/scala/FlightData.scala
+++ src/main/scala/FlightData.scala
@@ -1,10 +1,13 @@
 /* SimpleApp.scala */
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
-import org.apache.spark.SparkConf
+import org.apache.spark-3.2.1.SparkContext
+import org.apache.spark-3.2.1.SparkContext._
+import org.apache.spark-3.2.1.SparkConf
+import spark.implicits._
+// val spark = SparkSession.builder().appName("Flight Data").master("local").getOrCreate
 
 object FlightData {
   def main(args: Array[String]) {
     println("hello")
+    val df = spark.read.csv("data/flightData.csv")  
   }
 }
