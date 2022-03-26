d582db91102290daa147b856b1eebc3ff9dbd164
Tarin <tarin.eccleston@gmail.com>
2022-03-26 13:04:10 +1300

    Init

---
+++ flightdata.sbt
@@ -0,0 +1,7 @@
+name := "Flight Data"
+
+version := "1.0"
+
+scalaVersion := "2.13.8"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "3.2.1"
---
+++ src/main/scala/FlightData.scala
@@ -0,0 +1,10 @@
+/* SimpleApp.scala */
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
+object FlightData {
+  def main(args: Array[String]) {
+    println("hello")
+  }
+}
